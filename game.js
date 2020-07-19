650434b1685bf55e9a98846a65a4c3ccb9ce4de6
Claudio Gallo <cgb1408@outlook.com>
2020-07-19 15:48:29 -0600

    Added Highscores files and edited questions

--- game.js
+++ game.js
@@ -14,7 +14,7 @@ let availableQuesions = [];
 let questions = [];
 
 fetch(
-    'https://opentdb.com/api.php?amount=10&category=9&difficulty=medium&type=multiple'
+    'https://opentdb.com/api.php?amount=10&category=18&difficulty=easy&type=multiple'
 )
     .then((res) => {
         return res.json();
@@ -48,7 +48,7 @@ fetch(
 
 //CONSTANTS
 const CORRECT_BONUS = 10;
-const MAX_QUESTIONS = 3;
+const MAX_QUESTIONS = 5;
 
 startGame = () => {
     questionCounter = 0;
